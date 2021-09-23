a2cc4efb0182994c246a871bdf025aa1092a75d7
billkuojhgf <billkuojhgf@gmail.com>
2021-09-24 03:06:50 +0800

    qCSI前端完成+ qcsi+rox的API

--- fhir_WebClient/action.js
+++ fhir_WebClient/action.js
@@ -2,12 +2,25 @@ Vue.use(VueResource)
 const app = new Vue({
     el: "#app",
     data: {
-        showTab:[false, false],
         isClick: false,
         hasData: false,
         model: "",
         param_id: "test-03121002",
         predict_value: 0.00,
+        qcsi_value: {
+            "fio2": {
+                "date": "", 
+                "value": 0
+            }, 
+            "respiratory rate": {
+                "date": "", 
+                "value": 0
+            }, 
+            "spo2": {
+                "date": "", 
+                "value": 0
+            }
+        },
         results: {}
     },
     methods: {
@@ -16,11 +29,17 @@ const app = new Vue({
                 app.hasData = true
                 //callback successed
                 result_json = response.data
-                if (result_json.hasOwnProperty("predict_value")) {
-                    app.predict_value = response.data.predict_value
-                    delete result_json["predict_value"]
+                if (app.model == "qcsi")
+                    app.qcsiSearchQuery(result_json)
+                else if (app.model == "rox")
+                    app.roxSearchQuery(result_json)
+                else {
+                    if (result_json.hasOwnProperty("predict_value")) {
+                        app.predict_value = response.data.predict_value
+                        delete result_json["predict_value"]
+                    }
+                    app.results = result_json
                 }
-                app.results = result_json
             }, response => {
                 //callback errored
                 app.hasData = false
@@ -28,14 +47,47 @@ const app = new Vue({
                 app.isClick = true
             })
         },
+        qcsiSearchQuery: (results) => {
+            for (let key in results) {
+                switch (key) {
+                    case 'fio2':
+                        if (results[key].value <= 2)
+                            results[key].value = 0
+                        else if (results[key].value >= 5)
+                            results[key].value = 5
+                        else
+                            results[key].value = 4
+                        break;
+                    case 'respiratory rate':
+                        if (results[key].value <= 22)
+                            results[key].value = 0
+                        else if (results[key].value >= 28)
+                            results[key].value = 2
+                        else
+                            results[key].value = 1
+                        break
+                    case 'spo2':
+                        if (results[key].value > 92)
+                            results[key].value = 0
+                        else if (results[key].value <= 88)
+                            results[key].value = 5
+                        else
+                            results[key].value = 2
+                        
+                }
+            }
+            app.qcsi_value = results
+        },
+        radio_qcsi_rox_clicks: (type, key, value) => {
+            if (type === 'qcsi')
+                app.qcsi_value[key].value = value
+        },
+        roxSearchQuery: (results) => {
+
+        },
+        
         radioclicks: (key, bool) => {
             app.results[key].value = bool
-        },
-        tabClicks: (key) => {
-            for (i = 0; i < app.showTab.length; i++) {
-                app.showTab[i] = false
-            }
-            app.showTab[key] = true
         }
     }
 })
